871ba675555981d73cc942e375ff86331ed36cf7
Shuto Horie <j9t65cghv2@privaterelay.appleid.com>
2026-05-24 23:02:44 +0900

    Implement step2 of 141

---
+++ problems/141_linked_list_cycle/step2.py
@@ -0,0 +1,64 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# input:
+# ListNode(val=3, next=(pointer_2))
+# -> ListNode(val=2, next=(pointer_0)),
+# -> ListNode(val=0, next=(pointer_-4))
+# -> ListNode(val=-4, next=(pointer_2))
+
+# output: true
+
+# Memo
+# Strategy 1. Check all nodes from head and save the nodes to recognize the node has been already visited.
+# If duprication occurs, the list has circle. 
+# This approach is easy to implement but it requires O(n) space complexity.
+
+
+# Strategy 2. Use two pointers that speeds are diffrent. First pointer moves 1 node in 1 step
+# and second pointer moves 2 nodes in 1 step.
+# This approach is a bit tricky but it requires O(1) space complexity.
+
+# Example. A -> B -> C -> D -> A
+# First Pointer:  A -> B -> C -> D
+# Second Pointer: B -> D -> B -> D
+
+from typing import Optional
+
+
+class Solution:
+
+    # Strategy 1
+    def hasCycle1(self, head: Optional["ListNode"]) -> bool:
+        visited_node = set()
+
+        while(head is not None):
+            # 1. Check      
+            if head in visited_node:
+                return True
+            # 2. Update vistied_node
+            visited_node.add(head)
+            # 3. Update head
+            head = head.next
+        
+        return False
+
+    # Strategy 2
+    def hasCycle(self, head: Optional["ListNode"]) -> bool:
+        
+        pointer1 = head
+        pointer2 = head.next if head is not None else None
+
+        while(pointer2 is not None):
+            # 1. Check
+            if pointer1 == pointer2:
+                return True
+
+            # 2. Update
+            pointer1 = pointer1.next
+            pointer2 = None if pointer2.next is None else pointer2.next.next
+
+        return False
